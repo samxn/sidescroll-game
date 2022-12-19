6be6889c5d286b3f91d5971cd8cc3811e0ebb76b
Samin <86338112+samxn@users.noreply.github.com>
2022-12-18 23:59:51 -0500

    imported input.js to script.js and added keys to input.js

--- input.js
+++ input.js
@@ -1,7 +1,30 @@
-//capture and keep track of user input
+export class InputHandler {
+  //capture and keep track of user input
 
-class InputHandler {
   constructor() {
     this.keys = [];
+    window.addEventListener("keydown", (e) => {
+      if (
+        (e.key === "ArrowDown" ||
+          e.key === "ArrowUp" ||
+          e.key === "ArrowLeft" ||
+          e.key === "ArrowRight" ||
+          e.key === "Enter") &&
+        this.keys.indexOf(e.key) === -1
+      ) {
+        this.keys.push(e.key);
+      }
+    });
+    window.addEventListener("keyup", (e) => {
+      if (
+        e.key === "ArrowDown" ||
+        e.key === "ArrowUp" ||
+        e.key === "ArrowLeft" ||
+        e.key === "ArrowRight" ||
+        e.key === "Enter"
+      ) {
+        this.keys.splice(this.keys.indexOf(e.key), 1);
+      }
+    });
   }
 }
