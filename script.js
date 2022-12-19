729250913c47e759b223e65cf3b3de94888aa9c0
Samin <86338112+samxn@users.noreply.github.com>
2022-12-18 23:17:05 -0500

    updated script.js contents and created player.js file

--- script.js
+++ script.js
@@ -3,4 +3,14 @@ window.addEventListener("load", function () {
   const ctx = canvas.getContext("2d");
   canvas.width = 500;
   canvas.height = 500;
+
+  class Game {
+    //all project logic will go through this
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+    }
+    update() {}
+    draw() {}
+  }
 });
