6a3b8f717ce9af41d67037ebd8d798e47f1e841c
Samin <86338112+samxn@users.noreply.github.com>
2022-12-19 00:08:39 -0500

    Updated player.js so that player cannot move off screen

--- player.js
+++ player.js
@@ -7,10 +7,18 @@ export class Player {
     this.x = 0; //player position
     this.y = this.game.height - this.height; //player position
     this.image = document.getElementById("player");
+    this.speed = 0;
+    this.maxSpeed = 10;
   }
   update(input) {
-    if (input.includes("ArrowRight")) this.x++;
-    else if (input.includes("ArrowLeft")) this.x--;
+    // horizontal movement
+    this.x += this.speed;
+    if (input.includes("ArrowRight")) this.speed = this.maxSpeed;
+    else if (input.includes("ArrowLeft")) this.speed = -this.maxSpeed;
+    else this.speed = 0;
+    if (this.x < 0) this.x = 0;
+    if (this.x > this.game.width - this.width)
+      this.x = this.game.width - this.width;
   }
   draw(context) {
     context.drawImage(
