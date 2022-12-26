4dbdacadf69703f16afbe5ddc80ade0f563aa43e
Samin <86338112+samxn@users.noreply.github.com>
2022-12-26 01:41:50 -0500

    Added fire effect when user presses enter to roll

--- particles.js
+++ particles.js
@@ -6,7 +6,7 @@ class Particle {
   update() {
     this.x -= this.speedX + this.game.speed;
     this.y -= this.speedY;
-    this.size *= 0.95;
+    this.size *= 0.95; // adjust for longer or shorter fire trail
     if (this.size < 0.5) this.markedForDeletion = true;
   }
 }
@@ -31,4 +31,34 @@ export class Dust extends Particle {
 
 export class Splash extends Particle {}
 
-export class Fire extends Particle {}
+export class Fire extends Particle {
+  constructor(game, x, y) {
+    super(game);
+    this.image = document.getElementById("fire");
+    this.size = Math.random() * 50 + 50; // size of fire
+    this.x = x;
+    this.y = y;
+    this.speedX = 1;
+    this.speedY = 1;
+    this.angle = 0;
+    this.va = Math.random() * 0.2 - 0.2; // make fire faster or slower
+  }
+  update() {
+    super.update();
+    this.angle += this.va;
+    this.x += Math.sin(this.angle * 5);
+  }
+  draw(context) {
+    context.save();
+    context.translate(this.x, this.y);
+    context.rotate(this.angle);
+    context.drawImage(
+      this.image,
+      -this.size * 0.5,
+      -this.size * 0.5,
+      this.size,
+      this.size
+    );
+    context.restore();
+  }
+}
