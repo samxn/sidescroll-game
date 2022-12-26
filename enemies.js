d61cdc4ab1b738f15145aeff74780a0c6a70b3ce
Samin <86338112+samxn@users.noreply.github.com>
2022-12-25 23:59:37 -0500

    Brought fly enemies on screen at random

---
+++ enemies.js
@@ -0,0 +1,56 @@
+class Enemy {
+  constructor() {
+    this.frameX = 0;
+    this.frameY = 0;
+    this.fps = 20;
+    this.frameInterval = 1000 / this.fps;
+    this.frameTimer = 0;
+  }
+  update(deltaTime) {
+    //movement
+    this.x -= this.speedX;
+    this.y += this.speedY;
+    if (this.frameTimer > this.frameInterval) {
+      this.frameTimer = 0;
+      if (this.frameX < this.maxFrame) this.frameX++;
+      else this.frameX = 0;
+    } else {
+      this.frameTimer += deltaTime;
+    }
+  }
+  draw(context) {
+    context.drawImage(
+      this.image,
+      this.frameX * this.width,
+      0,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
+export class FlyingEnemy extends Enemy {
+  constructor(game) {
+    super();
+    this.game = game;
+    this.width = 60;
+    this.height = 44;
+    this.x = this.game.width;
+    this.y = Math.random() * this.game.height * 0.5;
+    this.speedX = 2;
+    this.speedY = 0;
+    this.maxFrame = 5;
+    this.image = document.getElementById("enemy_fly");
+  }
+  update(deltaTime) {
+    super.update(deltaTime);
+  }
+}
+
+export class GroundEnemy extends Enemy {}
+
+export class ClimbingEnemy extends Enemy {}
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import { Player } from "./player.js";
 import { InputHandler } from "./input.js";
 import { Background } from "./background.js";
+import { FlyingEnemy, ClimbingEnemy, GroundEnemy } from "./enemies.js";
 
 window.addEventListener("load", function () {
   const canvas = document.getElementById("canvas1");
@@ -13,20 +14,39 @@ window.addEventListener("load", function () {
     constructor(width, height) {
       this.width = width;
       this.height = height;
-      this.groundMargin = 50; // player margin used to match player to background
+      this.groundMargin = 80; // player margin used to match player to background
       this.speed = 0;
       this.maxSpeed = 3;
       this.background = new Background(this);
       this.player = new Player(this);
       this.input = new InputHandler();
+      this.enemies = [];
+      this.enemyTimer = 0;
+      this.enemyInterval = 1000;
     }
     update(deltaTime) {
       this.background.update();
       this.player.update(this.input.keys, deltaTime);
+      // handleEnemies
+      if (this.enemyTimer > this.enemyInterval) {
+        this.addEnemy();
+        this.enemyTimer = 0;
+      } else {
+        this.enemyTimer += deltaTime;
+      }
+      this.enemies.forEach((enemy) => {
+        enemy.update(deltaTime);
+      });
     }
     draw(context) {
       this.background.draw(context);
       this.player.draw(context);
+      this.enemies.forEach((enemy) => {
+        enemy.draw(context);
+      });
+    }
+    addEnemy() {
+      this.enemies.push(new FlyingEnemy(this));
     }
   }
   const game = new Game(canvas.width, canvas.height);
