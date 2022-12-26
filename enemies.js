81468f2a49554f2d07a511349727cd9199e99b75
Samin <86338112+samxn@users.noreply.github.com>
2022-12-26 00:26:48 -0500

    Added spider enemy and its web string

--- enemies.js
+++ enemies.js
@@ -73,4 +73,31 @@ export class GroundEnemy extends Enemy {
   }
 }
 
-export class ClimbingEnemy extends Enemy {}
+export class ClimbingEnemy extends Enemy {
+  constructor(game) {
+    super();
+    this.game = game;
+    this.game = game;
+    this.width = 120;
+    this.height = 144;
+    this.x = this.game.width;
+    this.y = Math.random() * this.game.height * 0.5;
+    this.image = document.getElementById("enemy_spider_big");
+    this.speedX = 0;
+    this.speedY = Math.random() > 0.5 ? 1 : -1;
+    this.maxFrame = 5;
+  }
+  update(deltaTime) {
+    super.update(deltaTime);
+    if (this.y > this.game.height - this.height - this.game.groundMargin)
+      this.speedY *= -1;
+    if (this.y < -this.height) this.markedForDeletion = true;
+  }
+  draw(context) {
+    super.draw(context);
+    context.beginPath();
+    context.moveTo(this.x + this.width / 2, 0);
+    context.lineTo(this.x + this.width / 2, this.y + 50);
+    context.stroke();
+  }
+}
