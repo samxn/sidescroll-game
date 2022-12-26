16b2cd9519cd9b79575254a4d3f2af138e02ab64
Samin <86338112+samxn@users.noreply.github.com>
2022-12-26 00:16:44 -0500

    Added plant enemy

--- enemies.js
+++ enemies.js
@@ -58,6 +58,19 @@ export class FlyingEnemy extends Enemy {
   }
 }
 
-export class GroundEnemy extends Enemy {}
+export class GroundEnemy extends Enemy {
+  constructor(game) {
+    super();
+    this.game = game;
+    this.width = 60;
+    this.height = 87;
+    this.x = this.game.width;
+    this.y = this.game.height - this.height - this.game.groundMargin;
+    this.image = document.getElementById("enemy_plant");
+    this.speedX = 0;
+    this.speedY = 0;
+    this.maxFrame = 1;
+  }
+}
 
 export class ClimbingEnemy extends Enemy {}
--- script.js
+++ script.js
@@ -48,6 +48,8 @@ window.addEventListener("load", function () {
       });
     }
     addEnemy() {
+      if (this.speed > 0 && Math.random() < 0.5)
+        this.enemies.push(new GroundEnemy(this));
       this.enemies.push(new FlyingEnemy(this));
     }
   }
