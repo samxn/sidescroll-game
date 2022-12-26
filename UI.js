dc7fa340e6e55b06ebcf799b8164f61ee42fbedb
Samin <86338112+samxn@users.noreply.github.com>
2022-12-26 16:03:21 -0500

    Added and styled win and lose message and fixed timer to count seconds

--- UI.js
+++ UI.js
@@ -2,13 +2,52 @@ export class UI {
   constructor(game) {
     this.game = game;
     this.fontSize = 30;
-    this.fontFamily = "Helveica";
+    this.fontFamily = "Helvetica";
   }
   draw(context) {
-    context.font = this.fontSize + "px" + this.fontFamily;
+    context.save();
+    context.shadowOffsetX = 2;
+    context.shadowOffsetY = 2;
+    context.shadowColor = "white";
+    context.shadowBlur = 0;
+    context.font = this.fontSize + "px " + this.fontFamily; // space after px matters or wont work
     context.textAlign = "left";
     context.fillStyle = this.game.fontColor;
     // score
     context.fillText("Score: " + this.game.score, 20, 50);
+    // timer
+    context.font = this.fontSize * 0.8 + "px " + this.fontFamily;
+    context.fillText("Time: " + (this.game.time * 0.001).toFixed(1), 20, 80);
+    // game over text
+    if (this.game.gameOver) {
+      context.textAlign = "center";
+      context.font = this.fontSize * 2 + "px " + this.fontFamily;
+      if (this.game.score > 5) {
+        context.fillText(
+          "YOU WIN",
+          this.game.width * 0.5,
+          this.game.height * 0.5 - 20
+        );
+        context.font = this.fontSize * 0.7 + "px " + this.fontFamily;
+        context.fillText(
+          "Creatures of the night are afraid of you...",
+          this.game.width * 0.5,
+          this.game.height * 0.5 + 20
+        );
+      } else {
+        context.fillText(
+          "YOU LOSE",
+          this.game.width * 0.5,
+          this.game.height * 0.5 - 20
+        );
+        context.font = this.fontSize * 0.7 + "px " + this.fontFamily;
+        context.fillText(
+          "Better luck next time...",
+          this.game.width * 0.5,
+          this.game.height * 0.5 + 20
+        );
+      }
+    }
+    context.restore();
   }
 }
--- script.js
+++ script.js
@@ -31,10 +31,15 @@ window.addEventListener("load", function () {
       this.debug = false;
       this.score = 0;
       this.fontColor = "black";
+      this.time = 0;
+      this.maxTime = 10000;
+      this.gameOver = false;
       this.player.currentState = this.player.states[0];
       this.player.currentState.enter();
     }
     update(deltaTime) {
+      this.time += deltaTime;
+      if (this.time > this.maxTime) this.gameOver = true;
       this.background.update();
       this.player.update(this.input.keys, deltaTime);
       // handle enemies
@@ -94,7 +99,7 @@ window.addEventListener("load", function () {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     game.update(deltaTime);
     game.draw(ctx);
-    requestAnimationFrame(animate);
+    if (!game.gameOver) requestAnimationFrame(animate);
   }
   animate(0);
 });
