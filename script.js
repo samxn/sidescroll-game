967a991a61fe84af7af61d8a812c64c250451891
Samin <86338112+samxn@users.noreply.github.com>
2022-12-26 00:48:36 -0500

    Implemented score feature which increases as player collides with enemies

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ import { Player } from "./player.js";
 import { InputHandler } from "./input.js";
 import { Background } from "./background.js";
 import { FlyingEnemy, ClimbingEnemy, GroundEnemy } from "./enemies.js";
+import { UI } from "./UI.js";
 
 window.addEventListener("load", function () {
   const canvas = document.getElementById("canvas1");
@@ -20,11 +21,13 @@ window.addEventListener("load", function () {
       this.background = new Background(this);
       this.player = new Player(this);
       this.input = new InputHandler(this);
+      this.UI = new UI(this);
       this.enemies = [];
       this.enemyTimer = 0;
       this.enemyInterval = 1000;
       this.debug = true;
       this.score = 0;
+      this.fontColor = "black";
     }
     update(deltaTime) {
       this.background.update();
@@ -48,6 +51,7 @@ window.addEventListener("load", function () {
       this.enemies.forEach((enemy) => {
         enemy.draw(context);
       });
+      this.UI.draw(context);
     }
     addEnemy() {
       if (this.speed > 0 && Math.random() < 0.5)
